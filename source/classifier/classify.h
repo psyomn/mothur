b0a6f9016f26f98ae4e4c29a6e9f8a41c1418d6a
Simon Symeonidis <lethaljellybean@gmail.com>
2024-04-24 23:13:59 -0400

    wip

--- source/classifier/classify.h
+++ source/classifier/classify.h
@@ -14,7 +14,7 @@
 /* This class is a parent to bayesian, knn.  */
 
 #include "mothur.h"
-#include "searchdatabase.hpp"
+#include "datastructures/searchdatabase.hpp"
 #include "phylotree.h"
 
 class Sequence;
@@ -26,34 +26,34 @@ class Classify {
 public:
 	Classify();
 	virtual ~Classify(){};
-    
+
 	virtual string getTaxonomy(Sequence*, string&, bool&) = 0;
     int getMaxLevel() { return maxLevel; }
-	
+
 	virtual void setDistName(string s) {} //for knn, so if distance method is selected with knn you can create the smallest distance file in the right place.
-    
-	
+
+
 protected:
-    
+
 	map<string, string> taxonomy;  //name maps to taxonomy
 	map<string, int>::iterator itTax;
 	map<string, string>::iterator it;
     SearchDatabase* database;
 	PhyloTree* phyloTree;
-	
+
 	string taxFile, templateFile;
 	vector<string> names;
 	int threadID, numLevels, numTaxa, maxLevel;
 	bool flip, shortcuts;
     MothurOut* m;
-	
+
 	int readTaxonomy(string);
 	vector<string> parseTax(string);
     double getLogExpSum(vector<double>, int&);
     bool checkReleaseDate(vector<ifstream*>&, string);
     virtual void generateDatabaseAndNames(string, string, string, int, float, float, float, float, string);
 
-	
+
 };
 
 /**************************************************************************************************/
